af8d04f0a4677a564b78962b22a9bfce866ec94b
Henry Emeka Obadoni <47888360+jediiry@users.noreply.github.com>
2024-03-02 19:19:46 +0000

    implemented repository methods

--- src/respositories/BaseRespository.ts
+++ src/respositories/BaseRespository.ts
@@ -1,5 +1,5 @@
 export interface IBase {
-  create(data: any): Promise<object>;
+  create(data: object): Promise<object>;
   update(filter: object, data: object): Promise<object>;
   delete(filter: object): Promise<object>;
   getUnique(filter: object): Promise<object>;
@@ -8,36 +8,35 @@ export interface IBase {
 }
 
 export class Base implements IBase {
-
   private modelName: string;
   private db: any;
-  
+
   constructor(db: any, modelName: string) {
     this.db = db;
     this.modelName = modelName;
   }
 
-  public async create(data: any): Promise<object> {
-    return {};
+  public async create(data: object): Promise<object> {
+    return await this.db[this.modelName].create(data);
   }
 
-  public async update(filter: object, data: object): Promise<object> {
-    return {};
+  public async update(data: object): Promise<object> {
+    return await this.db[this.modelName].update(data);
   }
 
   public async delete(filter: object): Promise<object> {
-    return {};
+    return await this.db[this.modelName].delete(filter);
   }
 
   public async getUnique(filter: object): Promise<object> {
-    return {};
+    return await this.db[this.modelName].findUnique(filter);
   }
 
   public async get(filter: object): Promise<object> {
-    return {};
+    return await this.db[this.modelName].findFirst(filter);
   }
 
   public async getAll(filter: object): Promise<Array<object>> {
-    return [];
+    return await this.db[this.modelName].findMany(filter);
   }
 }
